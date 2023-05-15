5e15cc0ea0af13824f169a15998aa64d0a7bdc4a
Emanuel.ps <emanuelpages.ps@gmail.com>
2023-05-15 17:52:44 -0300

    correcciones, agregado el empate y anulacion de botones despues de un ganador

--- scripts.js
+++ scripts.js
@@ -21,257 +21,223 @@ const playerCruz = document.createElement("p");
 //ciculo o cruz booleano true o false
 let circulo = false;
 let cruz = false;
-let buttonsList = 0; //acumulador, una vez que llega a nueve el juego esta empatad0
+let buttonsList = 0; //acumulador, una vez que llega a nueve el juego esta empatado
 
 for (const button of buttons) {
   button.addEventListener("click", function onClick() {
     console.log("click");
-    if (circulo == false && cruz === false) {
-      getCruz(button);
-    } else if (circulo == true && cruz == false) {
-      getCruz(button);
-    } else if (circulo == false && cruz == true) {
-      getCirculo(button);
-    }
+    circulo == false && cruz === false
+      ? getCruz(button)
+      : circulo == true && cruz == false
+      ? getCruz(button)
+      : circulo == false && cruz == true
+      ? getCirculo(button)
+      : null;
   });
 }
 
 const getCruz = (button) => {
-  if (button.innerHTML != "") {
-    return button.innerHTML;
-  } else if (button.innerHTML == "") {
-    circulo = false;
-    cruz = true;
-    button.innerHTML = "X";
-    button.value = "X";
-    buttonsList < 9 ? buttonsList++ : finishMessage();
-    console.log(buttonsList);
-    victoryRulesX();
-  }
+  button.innerHTML != ""
+    ? button.innerHTML
+    : ((circulo = false),
+      (cruz = true),
+      (button.innerHTML = "X"),
+      (button.value = "X"),
+      buttonsList < 9 ? buttonsList++ : buttonsList,
+      console.log(buttonsList),
+      victoryRulesX());
 };
 
 const getCirculo = (button) => {
-  if (button.innerHTML != "") {
-    return button.innerHTML;
-  } else if (button.innerHTML == "") {
-    cruz = false;
-    circulo = true;
-    button.innerHTML = "O";
-    button.value = "O";
-    buttonsList < 9 ? buttonsList++ : finishMessage();
-    console.log(buttonsList);
-    victoryRulesO();
-  }
+  button.innerHTML != ""
+    ? button.innerHTML
+    : ((cruz = false),
+      (circulo = true),
+      (button.innerHTML = "O"),
+      (button.value = "O"),
+      buttonsList < 9 ? buttonsList++ : buttonsList,
+      console.log(buttonsList),
+      victoryRulesO());
 };
 
 const victoryRulesX = () => {
-  if (buttons1.value == "X" && buttons2 == "X" && buttons3 == "X") {
-    buttons1.style.background='#007005';
-    buttons2.style.background='#007005';
-    buttons3.style.background='#007005';
+  if (buttons1.value == "X" && buttons2.value == "X" && buttons3.value == "X") {
+    buttons1.style.background = "#007005";
+    buttons2.style.background = "#007005";
+    buttons3.style.background = "#007005";
     VictoryX();
   } else if (
     buttons4.value == "X" &&
     buttons5.value == "X" &&
     buttons6.value == "X"
   ) {
-    buttons4.style.background='#007005';
-    buttons5.style.background='#007005';
-    buttons6.style.background='#007005';
+    buttons4.style.background = "#007005";
+    buttons5.style.background = "#007005";
+    buttons6.style.background = "#007005";
     VictoryX();
-    
   } else if (
     buttons7.value == "X" &&
     buttons8.value == "X" &&
     buttons9.value == "X"
   ) {
-    buttons7.style.background='#007005';
-    buttons8.style.background='#007005';
-    buttons9.style.background='#007005';
+    buttons7.style.background = "#007005";
+    buttons8.style.background = "#007005";
+    buttons9.style.background = "#007005";
     VictoryX();
   } else if (
     buttons1.value == "X" &&
     buttons4.value == "X" &&
     buttons7.value == "X"
   ) {
-    buttons1.style.background='#007005';
-    buttons4.style.background='#007005';
-    buttons7.style.background='#007005';
+    buttons1.style.background = "#007005";
+    buttons4.style.background = "#007005";
+    buttons7.style.background = "#007005";
     VictoryX();
   } else if (
     buttons2.value == "X" &&
     buttons5.value == "X" &&
     buttons8.value == "X"
   ) {
-    buttons2.style.background='#007005';
-    buttons5.style.background='#007005';
-    buttons8.style.background='#007005';
+    buttons2.style.background = "#007005";
+    buttons5.style.background = "#007005";
+    buttons8.style.background = "#007005";
     VictoryX();
   } else if (
     buttons3.value == "X" &&
     buttons6.value == "X" &&
     buttons9.value == "X"
   ) {
-    buttons3.style.background='#007005';
-    buttons6.style.background='#007005';
-    buttons9.style.background='#007005';
+    buttons3.style.background = "#007005";
+    buttons6.style.background = "#007005";
+    buttons9.style.background = "#007005";
     VictoryX();
   } else if (
     buttons1.value == "X" &&
     buttons5.value == "X" &&
     buttons9.value == "X"
   ) {
-    buttons1.style.background='#007005';
-    buttons5.style.background='#007005';
-    buttons9.style.background='#007005';
+    buttons1.style.background = "#007005";
+    buttons5.style.background = "#007005";
+    buttons9.style.background = "#007005";
     VictoryX();
   } else if (
     buttons3.value == "X" &&
     buttons5.value == "X" &&
     buttons7.value == "X"
   ) {
-    buttons3.style.background='#007005';
-    buttons5.style.background='#007005';
-    buttons7.style.background='#007005';
+    buttons3.style.background = "#007005";
+    buttons5.style.background = "#007005";
+    buttons7.style.background = "#007005";
     VictoryX();
+  } else {
+    Tie();
   }
 };
 
 const victoryRulesO = () => {
-  if (buttons1.value == "O" && buttons2 == "O" && buttons3 == "O") {
-    buttons1.style.background='#007005';
-    buttons2.style.background='#007005';
-    buttons3.style.background='#007005';
+  if (buttons1.value == "O" && buttons2.value == "O" && buttons3.value == "O") {
+    buttons1.style.background = "#007005";
+    buttons2.style.background = "#007005";
+    buttons3.style.background = "#007005";
     VictoryO();
   } else if (
     buttons4.value == "O" &&
     buttons5.value == "O" &&
     buttons6.value == "O"
   ) {
-    buttons4.style.background='#007005';
-    buttons5.style.background='#007005';
-    buttons6.style.background='#007005';
+    buttons4.style.background = "#007005";
+    buttons5.style.background = "#007005";
+    buttons6.style.background = "#007005";
     VictoryO();
-    
   } else if (
     buttons7.value == "O" &&
     buttons8.value == "O" &&
     buttons9.value == "O"
   ) {
-    buttons7.style.background='#007005';
-    buttons8.style.background='#007005';
-    buttons9.style.background='#007005';
+    buttons7.style.background = "#007005";
+    buttons8.style.background = "#007005";
+    buttons9.style.background = "#007005";
     VictoryO();
   } else if (
     buttons1.value == "O" &&
     buttons4.value == "O" &&
     buttons7.value == "O"
   ) {
-    buttons1.style.background='#007005';
-    buttons4.style.background='#007005';
-    buttons7.style.background='#007005';
+    buttons1.style.background = "#007005";
+    buttons4.style.background = "#007005";
+    buttons7.style.background = "#007005";
     VictoryO();
   } else if (
     buttons2.value == "O" &&
     buttons5.value == "O" &&
     buttons8.value == "O"
   ) {
-    buttons2.style.background='#007005';
-    buttons5.style.background='#007005';
-    buttons8.style.background='#007005';
+    buttons2.style.background = "#007005";
+    buttons5.style.background = "#007005";
+    buttons8.style.background = "#007005";
     VictoryO();
   } else if (
     buttons3.value == "O" &&
     buttons6.value == "O" &&
     buttons9.value == "O"
   ) {
-    buttons3.style.background='#007005';
-    buttons6.style.background='#007005';
-    buttons9.style.background='#007005';
+    buttons3.style.background = "#007005";
+    buttons6.style.background = "#007005";
+    buttons9.style.background = "#007005";
     VictoryO();
   } else if (
     buttons1.value == "O" &&
     buttons5.value == "O" &&
     buttons9.value == "O"
   ) {
-    buttons1.style.background='#007005';
-    buttons5.style.background='#007005';
-    buttons9.style.background='#007005';
+    buttons1.style.background = "#007005";
+    buttons5.style.background = "#007005";
+    buttons9.style.background = "#007005";
     VictoryO();
   } else if (
     buttons3.value == "O" &&
     buttons5.value == "O" &&
     buttons7.value == "O"
   ) {
-    buttons3.style.background='#007005';
-    buttons5.style.background='#007005';
-    buttons7.style.background='#007005';
+    buttons3.style.background = "#007005";
+    buttons5.style.background = "#007005";
+    buttons7.style.background = "#007005";
     VictoryO();
+  } else {
+    Tie();
   }
 };
 
 const VictoryX = () => {
   title.innerHTML = "GANASTE!";
   jugadorCruz++;
-  console.log(jugadorCruz);
+  for (const button of buttons) {
+    button.disabled = true;
+  }
+  
 };
 
 const VictoryO = () => {
   title.innerHTML = "GANASTE!";
   jugadorCirculo++;
-  buttonReset.onclick = function (){
-    resetValues();
+  for (const button of buttons) {
+    button.disabled = true;
   }
 };
 
-function finishMessage() {
-  console.log("aca");
-  buttonsList > 9 ? alert("Empate!") : (buttonsList = 0);
-}
+const Tie = (buttons) => {
+  if (buttonsList === 9) {
+    console.log('Empate');
+    title.innerHTML = "EMPATE!"
+    // mostrar mensaje de empate o hacer otra acción aquí
+  }
+  }
 
 
-const reset = (buttons) => {
-  for (const button of buttons) {
-  buttonReset.addEventListener("click", function onClick() {
-    
-      button.innerHTML = ''});
-    }
-    
-}
 
-//buttons es un array deberia recorrer todo el array y chequear primero si ya hay values asignados
-
-/* function cruzCirculoDecider() {
-  for (let i = 0; i < buttons.length; i++) {
-    console.log("llegsste");
-    if (buttons.innerHTML == "O") {
-      circulo = false;
-      cruz = true;
-      console.log("cruz");
-    } else if (buttons.innerHTML == "X") {
-      circulo = true;
-      cruz = false;
-      getCirculo(button);
-    } else {
-      getCruz(button);
-    }
+const reset = () => {
+    console.log('reset');
+      for (const button of buttons) {
+        button.innerHTML= '';
   }
 }
-
-const getCirculo = () => {
-  button.innerHTML = "O";
-  button.value = "O";
-}; */
-
-////
-/* function clickHandler(){
-    
-    console.log('click')
-        if((btn.textContent == 'O') || (btn.textContent == 'X')){
-            btn.textContent
-        } else{
-            play()
-            btn.textContent('i')
-        }
-    
-} */
